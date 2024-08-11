81ba9db311225c93322f63bc4ebd1aa1a07eb664
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-11 15:58:41 -0300

    estilização

--- src/assets/pages/Admim/Register/register.jsx
+++ src/assets/pages/Admim/Register/register.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import './_register.scss';
 import useRegister from '../../../hooks/useRegister';
+import Sucesso from '../sucesso/sucesso';
 
 
 const Register = () => {
@@ -8,15 +9,20 @@ const Register = () => {
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
   const [showError, setShowError] = useState(false);
+  const [success, setSuccess] = useState(false);
 
   const handleSubmit = async (event) => {
     event.preventDefault();
     try {
       await register(login, password);
-      // Redirecionar ou exibir uma mensagem de sucesso
+      setSuccess(true); // Exibir a mensagem de sucesso
     } catch {
       setShowError(true);
     }
+  };
+
+  const handleDismiss = () => {
+    setSuccess(false); // Fechar a mensagem de sucesso
   };
 
   useEffect(() => {
@@ -29,31 +35,37 @@ const Register = () => {
   }, [showError]);
 
   return (
-    <form className="form-control" onSubmit={handleSubmit}>
-      <p className="title">Registro</p>
-      <div className="input-field">
-        <input
-          required
-          className="input"
-          type="text"
-          placeholder="Usuário"
-          value={login}
-          onChange={(e) => setLogin(e.target.value)}
-        />
-      </div>
-      <div className="input-field">
-        <input
-          required
-          className="input"
-          type="password"
-          placeholder="Senha"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-        />
-      </div>
-      <button type="submit" className="submit-btn">Registrar</button>
-      {showError && <span className="error-message">{error}</span>}
-    </form>
+    <>
+      {success ? (
+        <Sucesso onDismiss={handleDismiss} />
+      ) : (
+        <form className="form-control" onSubmit={handleSubmit}>
+          <p className="title">Registro</p>
+          <div className="input-field">
+            <input
+              required
+              className="input"
+              type="text"
+              placeholder="Usuário"
+              value={login}
+              onChange={(e) => setLogin(e.target.value)}
+            />
+          </div>
+          <div className="input-field">
+            <input
+              required
+              className="input"
+              type="password"
+              placeholder="Senha"
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+          </div>
+          <button type="submit" className="submit-btn">Registrar</button>
+          {showError && <span className="error-message">{error}</span>}
+        </form>
+      )}
+    </>
   );
 };
 
