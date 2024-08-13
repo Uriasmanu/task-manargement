7fa1c992a9ee629913279032f3157dc6b6a98bf1
Manoela Urias <100574391+Uriasmanu@users.noreply.github.com>
2024-08-12 23:25:40 -0300

    feat: implementação da alteração de cor do status de acordo com o seu andamento

--- src/assets/components/Tarefa/Tarefa.jsx
+++ src/assets/components/Tarefa/Tarefa.jsx
@@ -4,15 +4,29 @@ import tarefa from '../../imagens/splitscreen.svg';
 import more from '../../imagens/more.svg';
 import play from '../../imagens/play.svg';
 
+const TransformStatus = (status) => {
+    switch (status) {
+        case 0:
+            return { className: "inicio", text: "Aguardando início" };
+        case 1:
+            return { className: "andamento", text: "Em andamento" };
+        case 2:
+            return { className: "concluido", text: "Concluído" };
+        default:
+            return { className: "", text: "" };
+    }
+}
+
 const Tarefa = ({ name, collaborator, status, timetrackers, update }) => {
+    const { className, text } = TransformStatus(status);
+    
     return (
         <div className="cards-tarefas">
             <div className="container">
                 <img src={tarefa} alt="icone-tarefa" />
                 <p>{name}</p>
 
-                <div className="status-ind"></div>
-                <p>{status}</p>
+                <div className={`status-ind ${className}`} data-status={text}></div>
 
                 <p>{update}</p>
             </div>
@@ -41,9 +55,9 @@ Tarefa.propTypes = {
         name: PropTypes.string,
 
     }),
-    status: PropTypes.number.isRequired, 
-    timetrackers: PropTypes.string, 
-    update: PropTypes.string.isRequired, 
+    status: PropTypes.number.isRequired,
+    timetrackers: PropTypes.string,
+    update: PropTypes.string.isRequired,
 };
 
 export default Tarefa;
